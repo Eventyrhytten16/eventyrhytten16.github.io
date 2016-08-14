b657ca897cf8105fd5f6fdf5bb92e1bf55d78dc7
Eventyrhyttte16 <anna@reland.dk>
2016-08-14 19:59:19 +0200

    lyder og nye billeder samt tekst

--- js/mouseoveraudio.js
+++ js/mouseoveraudio.js
@@ -30,13 +30,13 @@ function createsoundbite(sound){
 }
 
 var poca=createsoundbite("audio/Karloff.mp3");
-var roed=createsoundbite("audio/Obeydix.mp3");
+var roed=createsoundbite("audio/Trussi.mp3");
 var cock=createsoundbite("audio/Cockalot.mp3");
 var Moffa=createsoundbite("audio/Moffa.mp3");
-var tinder=createsoundbite("audio/Phree.mp3");
+var tinder=createsoundbite("audio/Tinderella.mp3");
 var skraev=createsoundbite("audio/Skraev.mp3");
 var peter=createsoundbite("audio/Spul.mp3");
-var piki=createsoundbite("audio/LOLita.mp3");
+var piki=createsoundbite("audio/Piki.mp3");
 var savfruen=createsoundbite("audio/Savfruen.mp3");
-var lokke=createsoundbite("audio/Darwin_Dick.mp3");
+var lokke=createsoundbite("audio/Hansi.mp3");
 var feer=createsoundbite("audio/Powerpuff.mp3");
